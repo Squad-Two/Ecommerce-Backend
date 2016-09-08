3136d47cfcfa8fc892c50dfb8bc7e374a4ce87ca
alex-braun <braunacb@gmail.com>
2016-09-08 13:56:24 -0400

    Create heroku app for backend.
    Changed for the correct filepaths.
    https://floating-lowlands-70144.herokuapp.com/

--- app/middleware/mongoose.js
+++ app/middleware/mongoose.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose');
 const uri = process.env.MONGODB_URI || 'mongodb://localhost/Ecommerce-Backend';
-// const uri = process.env.MONGOLAB_URI || 'mongodb://localhost/ecommerce-back';
+// const uri = process.env.MONGOLAB_URI || 'mongodb://localhost/Ecommerce-Backend';
 mongoose.Promise = global.Promise;
 mongoose.connect(uri);
 
