feffe16445f3c6010bb5a2086c9b5247e143d286
alex-braun <braunacb@gmail.com>
2016-12-09 21:59:13 -0500

    Add credentials for new bucket and old mongoose.
    credentials are not being committed, but mongoose uri function was
    moved.

--- app/middleware/mongoose.js
+++ app/middleware/mongoose.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const uri = process.env.MONGODB_URI || 'mongodb://localhost/Ecommerce-Backend';
-// const uri = process.env.MONGOLAB_URI || 'mongodb://localhost/Ecommerce-Backend';
+// const uri = process.env.MONGODB_URI || 'mongodb://localhost/Ecommerce-Backend';
+const uri = process.env.MONGOLAB_URI || 'mongodb://localhost/Ecommerce-Backend';
 mongoose.Promise = global.Promise;
 mongoose.connect(uri);
 
